583d0801461df518365280ba1c2a74b881b989d6
iraoscar <88068370+iraoscar@users.noreply.github.com>
2021-07-27 21:37:28 -0500

    Create quizzes.js

---
+++ src/quizzes.js
@@ -0,0 +1,84 @@
+export const quizzes = [
+  {
+    title: "Servidor en la nube",
+    questions: [
+      {
+        text: "¿Que es un servidor web?",
+        correctAnswer: "es un programa que utiliza HTTP",
+        incorrectAnswers: [
+          "Aplicacion Movil",
+          "Libreria linux",
+          "Menu de opciones"
+        ]
+      },
+      {
+        text: "¿Que es CodeSandbox?",
+        correctAnswer: "Plataforma de desarrollo de aplicaciones",
+        incorrectAnswers: [
+          "menu de investigacion",
+          "ventana",
+          "creacion de blogs"
+        ]
+      },
+      {
+        text: "¿que es software?",
+        correctAnswer: "Conjunto de programas",
+        incorrectAnswers: ["pagina web", "Buscador", "aplicaciones de windows"]
+      }
+    ]
+  },
+  {
+    title: "Software en el servidor",
+    questions: [
+      {
+        text: "¿Que es Linux?",
+        correctAnswer: " sistema operativo",
+        incorrectAnswers: [
+          "Herramienta de sandbox",
+          "Clase de nuevo proyecto",
+          "Linea de texto"
+        ]
+      },
+      {
+        text: "¿Que es react?",
+        correctAnswer: "biblioteca Javascript",
+        incorrectAnswers: ["Mando de java", "Codigo fuente", "Boton de proceso"]
+      },
+      {
+        text: "¿Que es un hipervinculo?",
+        correctAnswer: "Documento con hipertexto",
+        incorrectAnswers: [
+          "codigo URL",
+          "Gestion de proceso",
+          "Respaldo de fuente"
+        ]
+      }
+    ],
+    title: "CodeSandbox",
+    questions: [
+      {
+        text: "¿Ques es Codesandbox?",
+        correctAnswer: "Creacion de plantillas web",
+        incorrectAnswers: [
+          "Pagina de investigacion",
+          "creacion de texto",
+          "herramientas"
+        ]
+      },
+      {
+        text: "¿Que es una platilla?",
+        correctAnswer: "manera de escribir funciones",
+        incorrectAnswers: [
+          "organizacion",
+          "equipo de trabajo",
+          "desarrollo de programa"
+        ]
+      },
+      {
+        text: "¿Que es un usuario?",
+        correctAnswer: "conjunto de permisos y recursos asignados",
+        incorrectAnswers: ["Virus", "pagina web", "herramientas del servidor"]
+      }
+    ]
+  }
+];
